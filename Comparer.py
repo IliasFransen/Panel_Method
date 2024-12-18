d9ef3f28a1eaf29a01717f13a1a194c33bebc9de
IliasFransen <127627710+IliasFransen@users.noreply.github.com>
2024-12-18 09:17:03 +0100

    aaaaaaa

--- Comparer.py
+++ Comparer.py
@@ -17,21 +17,12 @@ x_list = data_xfoil_CP[:,0]
 y_list = data_xfoil_CP[:,1]
 Cp_list = data_xfoil_CP[:,2]
 
-Lengths = np.zeros(len(x_list)-1)
-cp_avg = np.zeros(len(x_list)-1)
+Cp_upper = Cp_list[:int(len(Cp_list)/2)+1]
+Cp_lower = np.flip(Cp_list[int(len(Cp_list)/2):])
 
-for i in range(len(x_list)-1):
-    Lengths[i] = np.sqrt((x_list[i+1]-x_list[i])**2+(y_list[i+1]-y_list[i])**2)
-    cp_avg[i] = (Cp_list[i]+Cp_list[i+1])/2
+cp_avg = np.zeros(len(Cp_upper)-1)
+for i in range(len(cp_avg)):
+    cp_avg[i] = (Cp_upper[i]+Cp_upper[i+1])/2
 
-
-#split in upper and lower part
-Lengths_upper = Lengths[: len(Lengths)//2+1]
-Lengths_lower = Lengths[len(Lengths)//2:]
-cp_avg_upper = cp_avg[: len(cp_avg)//2+1]
-cp_avg_lower = cp_avg[len(cp_avg)//2:]
-
-D_cp = -cp_avg_upper + cp_avg_lower
-
-plt.plot(x_list[:len(x_list)//2], D_cp)
+plt.plot(x_list[1:int(len(x_list)/2)], (-cp_avg+Cp_lower)[1:])
 plt.show()
